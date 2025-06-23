d04c4265d93a248fbeab9fbdbff33fa67b264c2d
poonam88 <63488541+poonam88@users.noreply.github.com>
2025-06-24 03:02:44 +0530

    Update whatsapp_utils.py

--- whatsapp_utils.py
+++ whatsapp_utils.py
@@ -1,11 +1,21 @@
 from twilio.rest import Client
 import os
 
-def send_whatsapp(to_number, message):
-    client = Client(os.getenv("TWILIO_SID"), os.getenv("TWILIO_AUTH_TOKEN"))
-    from_number = "whatsapp:+14155238886"  # Twilio Sandbox number
-    client.messages.create(
-        body=message,
-        from_=from_number,
-        to=to_number
-    )
+def send_whatsapp(message: str):
+    account_sid = os.getenv("TWILIO_SID")
+    auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+    from_whatsapp = os.getenv("TWILIO_PHONE")
+    to_whatsapp = os.getenv("USER_WHATSAPP")
+
+    client = Client(account_sid, auth_token)
+
+    try:
+        message = client.messages.create(
+            body=message,
+            from_=from_whatsapp,
+            to=to_whatsapp
+        )
+        return True
+    except Exception as e:
+        print("❌ Failed to send message:", e)
+        return False
