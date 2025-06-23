c4c456f216bba4f992e8afdb6f39e101d660d684
poonam88 <63488541+poonam88@users.noreply.github.com>
2025-06-24 03:19:57 +0530

    Update scheduled_tasks.py

--- scheduled_tasks.py
+++ scheduled_tasks.py
@@ -1,3 +1,11 @@
-def get_cron_expression_from_time(time_str):
-    hour, minute = time_str.split(":")
-    return f"{int(minute)} {int(hour)} * * *"  # daily at selected time
+from datetime import datetime, timedelta
+import pytz
+
+def get_utc_cron_from_local(reminder_time, timezone):
+    hour, minute = map(int, reminder_time.split(":"))
+    local_tz = pytz.timezone(timezone)
+    
+    local_time = local_tz.localize(datetime(2024, 1, 1, hour, minute))
+    utc_time = local_time.astimezone(pytz.utc)
+
+    return f"{utc_time.minute} {utc_time.hour} * * *"  # for Streamlit scheduler
